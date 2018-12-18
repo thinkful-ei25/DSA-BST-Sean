7e4afb66ba0b026a80b0b9a386a34cc44f70acae
Sean Nealon <signalflowsean@gmail.com>
2018-12-18 11:37:21 -0500

    insertion

--- BST.js
+++ BST.js
@@ -1,3 +1,5 @@
+'use strict'; 
+
 class BinarySearchTree{ 
     constructor(key=null, value=null, parent=null){ 
         this.key = null; 
@@ -6,4 +8,37 @@ class BinarySearchTree{
         this.left = null; 
         this.right = null; 
     }
+
+    insert(key, value){ 
+        if (this.key == null) { 
+            this.key = key; 
+            this.value = value; 
+        }
+        else if (key < this.key) { 
+            if (this.left == null) { 
+                this.left = new BinarySearchTree(key, value, this); 
+            }
+            else { 
+                this.left.insert(key, value); 
+            }
+        }
+        else if (key > this.key) { 
+            if (this.right == null) { 
+                this.right = new BinarySearchTree(key, value, this); 
+            }
+            else { 
+                this.right.insert(key, value); 
+            }
+        }
+    }
+
+    remove(){ 
+
+    }
+
+    find(){ 
+
+    }
 }
+
+module.exports = BinarySearchTree; 
