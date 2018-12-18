7e253068cbe7bbf242b703ab858f6b1ad419c636
Sean Nealon <signalflowsean@gmail.com>
2018-12-18 11:20:19 -0500

    BST constructor

---
+++ BST.js
@@ -0,0 +1,9 @@
+class BinarySearchTree{ 
+    constructor(key=null, value=null, parent=null){ 
+        this.key = null; 
+        this.value = null; 
+        this.parent = null; 
+        this.left = null; 
+        this.right = null; 
+    }
+}
